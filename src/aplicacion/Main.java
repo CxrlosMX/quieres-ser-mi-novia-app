d2b32916655e6e31afb78927ff50ab5cc602a865
CxrlosMX <LuisCRendon131@gmail.com>
2021-08-08 00:04:45 -0500

    Inicio del Proyecto-creacion paquetes y clases

---
+++ src/aplicacion/Main.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package aplicacion;
+
+/**
+ *
+ * @author: CxrlosMX
+ * @Git-Hub: https://github.com/CxrlosMX
+ * @Phone: 953-212-97-27
+ * @Email: LuisCRendon131@gmail.com
+ * @Date: 8/08/2021
+ * 
+ */
+public class Main {
+
+}
